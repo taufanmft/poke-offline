e750059fb3667fca624335eb14b2dcec7f2459e7
Taufan Taufik <90184133+taufanmft@users.noreply.github.com>
2022-04-18 17:43:22 +0700

    feat: custom query (#3)
    * feat: custom query
    
    * fix: fix re render
    
    * feat: hooks new feature
    
    * chore: cleanup
    
    * chore: cleanup
    
    Co-authored-by: Taufan Nicola <taufanderazor@gmail.com>

---
+++ src/hooks/useCustomQuery/index.ts
@@ -0,0 +1,64 @@
+import {get, set} from 'idb-keyval';
+import {OperationVariables, QueryHookOptions, useQuery} from '@apollo/client';
+import {DocumentNode, QueryResultExtend} from "./interfaces";
+import {useEffect, useState, useCallback} from "react";
+import hash from 'object-hash';
+import {ApolloQueryResult} from "@apollo/client/core/types";
+import {useOnline} from "../useOnline";
+
+const useCustomQuery = <TData, TVariables = OperationVariables>(
+    query: DocumentNode,
+    queryName: string,
+    options?: QueryHookOptions<TData, TVariables>,
+): QueryResultExtend<TData, TVariables> => {
+    const result = useQuery<TData, TVariables>(query, options);
+    const [resultData, setResultData] = useState<TData>();
+    const [finish, setFinish] = useState(false);
+    const [privateOptions, setPrivateOptions] = useState(options);
+    const [loading, setLoading] = useState(true);
+    const [from, setFrom] = useState('network');
+    const isOnline = useOnline();
+
+    const handleRefetch = useCallback(async (variables?: Partial<TVariables>): Promise<ApolloQueryResult<TData>> => {
+       setFinish(false);
+       setLoading(true)
+        setPrivateOptions(variables);
+       return result.refetch(variables);
+    }, [result]);
+
+    useEffect(() => {
+        if (result.data !== undefined) {
+            setLoading(false);
+        }
+        if (result.error) {
+            setLoading(false);
+        }
+    },[result.data, result.error]);
+
+   useEffect(() => {
+       (async function () {
+           if (!loading || !isOnline) {
+               const key = hash({privateOptions, queryName: queryName});
+               if (!finish) {
+                   if (result.data) {
+                       set(key, result.data);
+                       setResultData(result.data);
+                       setFinish(true);
+                       setLoading(false);
+                       setFrom('network');
+                   } else {
+                       const tania = await get(key);
+                       setResultData(tania)
+                       setFinish(true);
+                       setLoading(false);
+                       setFrom('cache');
+                   }
+               }
+           }
+       }());
+    }, [finish, isOnline, loading, options, privateOptions, result.data, result.loading]);
+
+    return {...result, data: resultData, from, refetch: handleRefetch, loading: loading};
+};
+
+export default useCustomQuery;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,23 @@
 import { HomeWrapper, PokemonsWrapper } from './styles';
-import {gql, useQuery} from "@apollo/client";
+import {gql} from "@apollo/client";
 import PokeCard from "./components/PokeCard";
+import useCustomQuery from "../../hooks/useCustomQuery";
+import {useState} from "react";
+
+
+type PokemonResponse = {
+    pokemons: {
+        results: Array<{
+            url: string;
+            name: string;
+            image: string;
+        }>
+    }
+};
 
 const GET_POKEMONS = gql`
     query pokemons($limit: Int, $offset: Int) {
         pokemons(limit: $limit, offset: $offset) {
-            count
-            next
-            previous
-            status
-            message
             results {
                 url
                 name
@@ -26,18 +34,28 @@ const gqlVariables = {
 
 
 const Home = () => {
-    const { data } = useQuery(GET_POKEMONS, {
+    const [offset, setOffset] = useState(31);
+    const { data, loading, from, refetch } = useCustomQuery<PokemonResponse>(GET_POKEMONS, 'GET_POKEMONS', {
         variables: gqlVariables,
     });
-    const pokemons = data?.pokemons?.results as Array<Pokemon> ?? [];
+    const pokemons = data?.pokemons?.results ?? [];
     return (
         <HomeWrapper>
             <h1>Poke Offline</h1>
+            {loading && <p>Loading...</p>}
+            <p>Accessing from: {from}</p>
             <PokemonsWrapper>
                 {pokemons.map(pokemon => (
                     <PokeCard img={pokemon.image} name={pokemon.name} />
                 ))}
             </PokemonsWrapper>
+            <button onClick={() => {
+                refetch({
+                    limit: 30,
+                    offset: offset,
+                })
+                setOffset(prevState => prevState + 30)
+            }}>Next Batch of Pokemons</button>
         </HomeWrapper>
     );
 };
